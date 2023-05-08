8fb7c418d99fd90a7141b6f45f479d2fbbee2bc6
dbsjq <tony92829@naver.com>
2023-05-08 14:32:12 +0900

    로비캐릭터 클래스 다시 추가

---
+++ Client/Private/Lobby_Character.cpp
@@ -0,0 +1,264 @@
+#include "stdafx.h"
+#include "..\Public\Lobby_Character.h"
+
+#include "GameInstance.h"
+#include "GameMode.h"
+
+CLobbyCharacter::CLobbyCharacter(ID3D11Device * pDevice, ID3D11DeviceContext * pContext)
+	: CGameObject(pDevice, pContext)
+{
+}
+
+CLobbyCharacter::CLobbyCharacter(const CLobbyCharacter & rhs)
+	: CGameObject(rhs)
+{
+}
+
+HRESULT CLobbyCharacter::Initialize_Prototype(_uint iModelID, _uint iAnimID)
+{
+	if (FAILED(__super::Initialize_Prototype()))
+		return E_FAIL;
+
+	m_iModelID = iModelID;
+	m_iAnimID = iAnimID;
+	return S_OK;
+}
+
+HRESULT CLobbyCharacter::Initialize(void * pArg)
+{
+	if (FAILED(__super::Initialize(pArg)))
+		return E_FAIL;
+
+	if (FAILED(Add_Components()))
+		return E_FAIL;
+
+	Init_AnimSystem();
+
+	m_pAnimSetCom->SetUp_Animation(m_eState, true);
+
+	return S_OK;
+}
+
+void CLobbyCharacter::Start()
+{
+}
+
+void CLobbyCharacter::Tick(_double TimeDelta)
+{
+	CGameInstance* pGame = CGameInstance::GetInstance();
+
+	if (pGame->InputKey(DIK_M) == KEY_STATE::TAP)
+	{
+		m_eState = STATE(m_eState + 1);
+		if (m_eState == STATE_END)
+			m_eState = STATE_IDLE;
+
+		m_pAnimSetCom->SetUp_Animation(m_eState, true);
+	}
+}
+
+void CLobbyCharacter::LateTick(_double TimeDelta)
+{
+}
+
+HRESULT CLobbyCharacter::Render()
+{
+	if (FAILED(__super::Render()))
+		return E_FAIL;
+
+	if (FAILED(SetUp_ShaderResources()))
+		return E_FAIL;
+
+	int iPass;
+	if (10.f <= ComputeCameraLength())
+		iPass = 3;
+	else
+		iPass = 4;
+
+
+	for (_uint i = 0; i < 6; ++i)
+	{
+		if (FAILED(m_pModelCom->SetUp_ShaderMaterialResource(m_pShaderCom, "g_DiffuseTexture", i, MyTextureType_DIFFUSE)))
+			return E_FAIL;
+
+		if (FAILED(m_pModelCom->SetUp_ShaderMaterialResource(m_pShaderCom, "g_NormalTexture", i, MyTextureType_NORMALS)))
+			return E_FAIL;
+
+		if (FAILED(m_pModelCom->SetUp_VertexTexture(m_pShaderCom, "g_VertexTexture", i)))
+			return E_FAIL;
+
+		if (i == 5)
+			m_pShaderCom->Begin(6); // Eye
+		else
+			m_pShaderCom->Begin(iPass);
+
+		m_pModelCom->Render(i);
+	}
+
+	return S_OK;
+}
+
+HRESULT CLobbyCharacter::RenderShadow()
+{
+	if (FAILED(__super::RenderShadow()))
+		return E_FAIL;
+
+	if (FAILED(Setup_ShadowShaderResource()))
+		return E_FAIL;
+
+	_uint iNumMeshes = m_pModelCom->Get_NumMeshes();
+	for (_uint i = 0; i < iNumMeshes; ++i)
+	{
+		if (FAILED(m_pModelCom->SetUp_VertexTexture(m_pShaderCom, "g_VertexTexture", i)))
+			return E_FAIL;
+
+		m_pShaderCom->Begin(5);
+		m_pModelCom->Render(i);
+	}
+
+	return S_OK;
+}
+
+void CLobbyCharacter::RenderGUI()
+{
+}
+
+HRESULT CLobbyCharacter::Add_Components()
+{
+	/* For.Com_Renderer*/
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, COMPONENT::RENDERER,
+		TEXT("Com_Renderer"), (CComponent**)&m_pRendererCom)))
+		return E_FAIL;
+
+	/* For.Com_Transform */
+	CTransform::TRANSFORM_DESC	TransformDesc;
+	ZeroMemory(&TransformDesc, sizeof TransformDesc);
+
+	TransformDesc.fMoveSpeed = 15.f;
+	TransformDesc.fRotationSpeed = XMConvertToRadians(90.f);
+
+	if (FAILED(__super::Add_Component(LEVEL_STATIC, COMPONENT::TRANSFORM,
+		TEXT("Com_Transform"), (CComponent**)&m_pMainTransform, &TransformDesc)))
+		return E_FAIL;
+
+	// For.Com_Shader_ModelAnim
+	if (FAILED(__super::Add_Component(LEVEL_ANIMTOOL, SHADER::MODELANIM,
+		TEXT("Com_Shader_ModelAnim"), (CComponent**)&m_pShaderCom)))
+		return E_FAIL;
+
+
+	/* For.Com_Model*/
+	if (FAILED(__super::Add_Component(LEVEL_ANIMTOOL, m_iModelID,
+		TEXT("Com_Model"), (CComponent**)&m_pModelCom)))
+		return E_FAIL;
+
+	/* For.Com_AnimSet_Base */
+	if (FAILED(__super::Add_Component(LEVEL_ANIMTOOL, m_iAnimID,
+		TEXT("Com_AnimSet_Base"), (CComponent**)&m_pAnimSetCom)))
+		return E_FAIL;
+
+	return S_OK;
+
+}
+
+void CLobbyCharacter::Init_AnimSystem()
+{
+	for (auto& pBone : m_pAnimSetCom->Get_Bones())
+		pBone->Set_TargetBone(m_pModelCom->Get_BonePtr(pBone->Get_Name()));
+
+	for (auto& pAnim : m_pAnimSetCom->Get_Animations())
+	{
+		const _tchar* szAnimName = pAnim->Get_Name();
+
+		for (auto& pChannel : pAnim->Get_Channels())
+		{
+			const _tchar* szChannelName = pChannel->Get_Name();
+			CBone* pBone = m_pAnimSetCom->Get_BonePtr(pChannel->Get_TargetBoneID());
+
+			pChannel->Set_Apply(true);
+		}
+	}
+}
+
+void CLobbyCharacter::Tick_State(_double TimeDelta)
+{
+	m_pAnimSetCom->Play_Animation(TimeDelta);
+	m_pAnimSetCom->Update_TargetBones();
+	m_pModelCom->Invalidate_CombinedMatrices();
+}
+
+HRESULT CLobbyCharacter::SetUp_ShaderResources()
+{
+	if (nullptr == m_pShaderCom)
+		return E_FAIL;
+	CGameInstance* pGameInstance = CGameInstance::GetInstance();
+
+	if (FAILED(m_pShaderCom->SetMatrix("g_WorldMatrix", &m_pMainTransform->Get_WorldMatrix())))
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->SetMatrix("g_ViewMatrix", &pGameInstance->Get_Transform_float4x4(CPipeLine::TS_VIEW))))
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->SetMatrix("g_ProjMatrix", &pGameInstance->Get_Transform_float4x4(CPipeLine::TS_PROJ))))
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->SetRawValue("g_vCamPosition", &pGameInstance->Get_CamPosition(), sizeof(_float4))))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+HRESULT CLobbyCharacter::Setup_ShadowShaderResource()
+{
+	if (nullptr == m_pShaderCom)
+		return E_FAIL;
+	CGameInstance* pGameInstance = CGameInstance::GetInstance();
+
+	if (FAILED(m_pShaderCom->SetMatrix("g_WorldMatrix", &m_pMainTransform->Get_WorldMatrix())))
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->SetMatrix("g_ViewMatrix", &pGameInstance->GetLightFloat4x4(LIGHT_MATRIX::LIGHT_VIEW))))
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->SetMatrix("g_ProjMatrix", &pGameInstance->GetLightFloat4x4(LIGHT_MATRIX::LIGHT_PROJ))))
+		return E_FAIL;
+
+	return S_OK;
+}
+
+CLobbyCharacter * CLobbyCharacter::Create(ID3D11Device * pDevice, ID3D11DeviceContext * pContext, _uint iModelID, _uint iAnimID)
+{
+	CLobbyCharacter* pInstance = new CLobbyCharacter(pDevice, pContext);
+
+	if (FAILED(pInstance->Initialize_Prototype(iModelID, iAnimID)))
+	{
+		MSG_BOX("Failed to Create : CTestVTF");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+
+CGameObject * CLobbyCharacter::Clone(void * pArg)
+{
+	CLobbyCharacter* pInstance = new CLobbyCharacter(*this);
+
+	if (FAILED(pInstance->Initialize(pArg)))
+	{
+		MSG_BOX("Failed to Clone : CTestVTF");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CLobbyCharacter::Free()
+{
+	__super::Free();
+	Safe_Release(m_pAnimSetCom);
+	Safe_Release(m_pModelCom);
+	Safe_Release(m_pMainTransform);
+	Safe_Release(m_pShaderCom);
+	Safe_Release(m_pRendererCom);
+}
