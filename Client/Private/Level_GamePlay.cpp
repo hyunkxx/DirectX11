4bdfee7ec8cff92ce406c9254f0c080d6fa17d4b
Jihyeon0 <wjswlqhd1@naver.com>
2023-05-12 18:53:01 +0900

    주석추가

--- Client/Private/Level_GamePlay.cpp
+++ Client/Private/Level_GamePlay.cpp
@@ -288,8 +288,8 @@ HRESULT CLevel_GamePlay::Ready_Layer_UI(const _tchar * pLayerTag)
 		return E_FAIL;
 	if (FAILED(pGameInstance->Add_GameObject(LEVEL_GAMEPLAY, OBJECT::UITAPT, pLayerTag, TEXT("UI_TapT"))))
 		return E_FAIL;
-	if (FAILED(pGameInstance->Add_GameObject(LEVEL_GAMEPLAY, OBJECT::UIMONSTER, pLayerTag, TEXT("UI_Monster"))))
-		return E_FAIL;
+	//if (FAILED(pGameInstance->Add_GameObject(LEVEL_GAMEPLAY, OBJECT::UIMONSTER, pLayerTag, TEXT("UI_Monster"))))
+	//	return E_FAIL;
 
 
 	return S_OK;
