3675aff2be97917fb085854c794448d68b72c70c
dbsjq <tony92829@naver.com>
2023-06-04 17:11:27 +0900

    ㅁㄴㅇㄹ

---
+++ Client/Private/P_Yangyang.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "..\Public\P_Yangyang.h"
+
+
+P_Yangyang::P_Yangyang()
+{
+}
+
+
+P_Yangyang::~P_Yangyang()
+{
+}
---
+++ Client/Public/P_Yangyang.h
@@ -0,0 +1,8 @@
+#pragma once
+class P_Yangyang
+{
+public:
+	P_Yangyang();
+	virtual ~P_Yangyang();
+};
+
