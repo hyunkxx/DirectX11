6bea2057a072f95794d146ed48f16a25375aa203
dbsjq <tony92829@naver.com>
2023-06-19 16:29:09 +0900

    1

--- Client/Private/M_Crownless_P3.cpp
+++ Client/Private/M_Crownless_P3.cpp
@@ -103,7 +103,7 @@ HRESULT CM_Crownless_P3::Initialize(void * pArg)
 	m_tMonsterInfo.eElement = ELMT_HAVOC;
 	m_tMonsterInfo.iLevel = 45;
 	m_tMonsterInfo.iExp = 0;
-	m_tMonsterInfo.fMaxHP = 48000.f;
+	m_tMonsterInfo.fMaxHP = 100000.f;
 	m_tMonsterInfo.fCurHP = m_tMonsterInfo.fMaxHP;
 	m_tMonsterInfo.fAttack = 750.f;
 	m_tMonsterInfo.fDefense = 600.f;
